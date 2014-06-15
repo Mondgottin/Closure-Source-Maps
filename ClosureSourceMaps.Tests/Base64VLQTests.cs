5a5f73964952c92d71abff7e87e053aa166dcb03
Victor Milovanov <lostfreeman@gmail.com>
2014-06-15 11:05:00 -0700

    + one Base64Vlq test

---
+++ ClosureSourceMaps.Tests/Base64VLQTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClosureSourceMaps.Tests
+{
+	using System.Text;
+
+	[TestClass]
+	public class Base64VLQTests
+	{
+		[TestMethod]
+		public void Test0To63()
+		{
+			for (int i = 0; i < 63; i++) {
+				TestEncodeDecode(i);
+			}
+		}
+
+		private void TestEncodeDecode(int value)
+		{
+			try {
+				var sb = new StringBuilder();
+				Base64Vlq.Encode(sb, value);
+				int result = Base64Vlq.Decode(sb.ToString());
+				Assert.AreEqual(value, result);
+			} catch (Exception e) {
+				throw new Exception("failed for value " + value, e);
+			}
+		}
+	}
+}
