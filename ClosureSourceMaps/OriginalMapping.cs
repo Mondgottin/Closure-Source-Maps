908c7eb8f93727ab37df5dee46c4272ce5002bfb
Diana <diana.sungatullina@gmail.com>
2014-07-13 21:01:36 +0400

    Add OriginalMapping class

---
+++ ClosureSourceMaps/OriginalMapping.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosureSourceMaps
+{
+    class OriginalMapping
+    {
+        // The original source file.
+        private string originalFile = "1";
+
+        // The line in the original file.
+        private int lineNumber = 2;
+
+        // The column number on the line.
+        private int columnPosition = 3;
+
+        // The original name of the identifier.
+        private string identifier = "4";
+    }
+}
